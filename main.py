47014f05f08f5683be40f5ba03a9bb981726483f
Amjad Balawi <amjad.balawi20@gmail.com>
2024-12-04 03:38:11 +0100

    first commit

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import os
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from groq import Groq
 from fastapi.middleware.cors import CORSMiddleware
+import requests
 # Initialize FastAPI app
 app = FastAPI()
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 # Allow specific origins (replace with the actual URL of your Angular frontend)
 origins = [
     "*"
@@ -28,6 +32,9 @@ class ChatRequest(BaseModel):
     messages: list
     model: str
 
+class TranslationRequest(BaseModel):
+    text: str
+    target_language: str
 
 @app.post("/api/chat-completion")
 async def chat_completion(request: ChatRequest):
@@ -46,6 +53,39 @@ async def chat_completion(request: ChatRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
+@app.post("/cultural-tips/")
+async def cultural_tips(location: str):
+    """Generate cultural tips based on location."""
+    try:
+        prompt = f"What are some cultural tips to avoid faux pas in {location}?"
+
+        chat_completion = client.chat.completions.create(
+            messages=[{"role": "user", "content": prompt}],
+            model="llama3-8b-8192"
+        )
+
+        return {"hidden_gems": chat_completion.choices[0].message.content}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/translate/")
+async def translate_text(request: TranslationRequest):
+    try:
+        query = f"""
+        Translate the following text from {request.text} to {request.target_language}:
+        "{request.text}"
+
+        Please only return the translated text. If the source language is the same as the destination language, return the original text without any other information.
+        """
+        chat_completion = client.chat.completions.create(
+            messages=[{"role": "user", "content": query}],
+            model="llama3-8b-8192"
+        )
+        return {"translate_text": chat_completion.choices[0].message.content}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.get("/api/hidden-gems")
 async def hidden_gems(location: str):
